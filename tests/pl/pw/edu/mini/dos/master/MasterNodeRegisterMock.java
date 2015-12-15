cc9ce0fdf1e03c66a2dd3f9eace755bf38360262
Dawid Pachowski <dawidppp@tlen.pl>
2015-12-15 08:32:53 -0800

    Added: Basic unit test.

---
+++ tests/pl/pw/edu/mini/dos/master/MasterNodeRegisterMock.java
@@ -0,0 +1,37 @@
+package pl.pw.edu.mini.dos.master;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import pl.pw.edu.mini.dos.communication.ErrorEnum;
+import pl.pw.edu.mini.dos.communication.masternode.*;
+
+import java.rmi.RemoteException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: asd
+ * Date: 12/15/15
+ * Time: 8:25 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MasterNodeRegisterMock implements MasterNodeInterface {
+    private static final Logger logger = LoggerFactory.getLogger(Master.class);
+
+    @Override
+    public ExecuteSQLOnNodeResponse executeSQLOnNode(ExecuteSQLOnNodeRequest executeSQLOnNodeRequest) throws RemoteException {
+        logger.info("executeSQLOnNode running...");
+        return new ExecuteSQLOnNodeResponse("String result from test", ErrorEnum.NO_ERROR);
+    }
+
+    @Override
+    public ExecuteCreateTablesResponse createTables(ExecuteCreateTablesRequest executeCreateTablesRequest) throws RemoteException {
+        logger.info("createTables running...");
+        return new ExecuteCreateTablesResponse(ErrorEnum.NO_ERROR);
+    }
+
+    @Override
+    public CheckStatusResponse checkStatus(CheckStatusRequest checkStatusRequest) throws RemoteException {
+        logger.info("checkStatus running...");
+        return new CheckStatusResponse(0, 0, 0);
+    }
+}
---
+++ tests/pl/pw/edu/mini/dos/master/MasterRegisterTest.java
@@ -0,0 +1,46 @@
+package pl.pw.edu.mini.dos.master;
+
+import org.junit.Test;
+import pl.pw.edu.mini.dos.communication.nodemaster.RegisterRequest;
+import pl.pw.edu.mini.dos.master.node.RegisteredNode;
+
+import java.rmi.RemoteException;
+import java.util.List;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: asd
+ * Date: 12/15/15
+ * Time: 8:24 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MasterRegisterTest {
+    Master master;
+    private MasterNodeRegisterMock node;
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+        master = new Master();
+        node = new MasterNodeRegisterMock();
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testRegisterOneNode() throws InterruptedException, RemoteException {
+        RegisterRequest registerRequest = new RegisterRequest(node);
+        master.register(registerRequest);
+
+        List<RegisteredNode> registeredNodes = master.nodeManager.getNodes();
+        assertNotNull(registeredNodes);
+        assertEquals(1, registeredNodes.size());
+        assertEquals(node, registeredNodes.get(0).getInterface());
+    }
+
+}
